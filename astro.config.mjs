c2b6d0b9be6dc9fd00d917f22d85ceea49b39416
Anton Hughes <kurrent93@gmail.com>
2023-06-13 15:45:26 +1200

    upgrade sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), sitemap()],
+  output: "server",
+  adapter: vercel()
 });
