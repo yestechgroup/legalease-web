a51c36e0ba6fa227e7d9be697256abd836229a85
Anton Hughes <kurrent93@gmail.com>
2023-06-16 16:06:45 +1200

    disable on submit

--- src/components/FeedbackForm.svelte
+++ src/components/FeedbackForm.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
   let responseMessage: string;
   let responseStatus: number;
+  let isDisabled = false;
 
   async function submit(e: SubmitEvent) {
     console.log("submit");
+    //disable input and button
+    isDisabled = true;
+    
+
     e.preventDefault();
     const formData = new FormData(e.currentTarget as HTMLFormElement);
     const response = await fetch("/api/feedback", {
@@ -71,12 +76,14 @@
             id="email"
             name="email"
             type="email"
+            disabled={isDisabled}
             autocomplete="email"
             required
             class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
             placeholder="Enter your email" />
           <button
             type="submit"
+            disabled={isDisabled}
             class="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
             >Notify me</button>
         {/if}
